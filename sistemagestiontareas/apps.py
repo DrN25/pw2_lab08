d267ccbe2d7b3d7d51e6cadd5501ee9b01937d48
msuarez <87085119+Suarezsh@users.noreply.github.com>
2024-06-25 00:05:02 -0500

    Delete sistemagestiontareas

--- sistemagestiontareas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SistemagestiontareasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sistemagestiontareas'
--- sistemagestiontareas/forms.py
+++
@@ -1,9 +0,0 @@
-
-
-from django import forms
-from .models import Proyecto
-
-class ProyectoForm(forms.ModelForm):
-    class Meta:
-        model = Proyecto
-        fields = ['nombre', 'descripcion', 'fecha_inicio', 'fecha_fin']
